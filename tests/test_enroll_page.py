ee97e7f6794351fb5e1dc47f646c476e05075673
lyubomir <l.mihaylov@abv.bg>
2023-12-29 18:11:26 +0200

    fix enroll page to adapt the centalized driver init

--- tests/test_enroll_page.py
+++ tests/test_enroll_page.py
@@ -1,7 +1,7 @@
 import time
 import urllib3.exceptions
 import pytest
-from selenium import webdriver
+from utilities.driver_handler import DriverHandler
 from utilities.config_reader import data, browsers_config, headless_mode_config
 from pages.enroll_page import EnrollPage
 from utilities.exception_logger import log_exception
@@ -10,21 +10,12 @@ from utilities.exception_logger import log_exception
 class DriveEnrollPage():
     _enroll_page : EnrollPage = None
     _DRIVER = None
-    _OPTIONS = None
 
-    def __init__(self, browser, URL, headless_mode):        
-        if(browser == 'Firefox'):
-            self._OPTIONS = webdriver.FirefoxOptions().add_argument('--headless=new') if headless_mode else None
-            firefox_service = webdriver.FirefoxService(log_output='geckodriver.log')
 
-            self._DRIVER = webdriver.Firefox(options=self._OPTIONS, service=firefox_service)
-        elif(browser == 'Edge'):
-            self._OPTIONS = webdriver.EdgeOptions().add_argument('--headless=new') if headless_mode else None
-            edge_service = webdriver.FirefoxService(log_output='geckodriver.log')
-
-            self._DRIVER = webdriver.Edge(options=self._OPTIONS, service=edge_service)
-
+    def __init__(self, driver, URL):
+        self._DRIVER = driver
         self._enroll_page = EnrollPage(self._DRIVER, URL)
+
 
     # Other
     def go_to_original_tab(self, curr_window_handle):        
@@ -103,33 +94,34 @@ class TestEnrollPage():
     @staticmethod
     def test_form():
         try:
-            drive_test_enroll_page.type_into_first_name_field('John') \
-                        .type_into_last_name_field('Hale') \
-                        .type_into_email_name_field('j.haile@gmail.com') \
-                        .type_into_mobile_name_field('0885666111') \
-                        .type_into_country_name_field('Bulgaria') \
-                        .type_into_city_name_field('Sofia') \
-                        .type_into_message_name_field('Test message') \
-                        .type_into_code_name_field('code')
+            drive_enroll_page.type_into_first_name_field('John') \
+                             .type_into_last_name_field('Hale') \
+                             .type_into_email_name_field('j.haile@gmail.com') \
+                             .type_into_mobile_name_field('0885666111') \
+                             .type_into_country_name_field('Bulgaria') \
+                             .type_into_city_name_field('Sofia') \
+                             .type_into_message_name_field('Test message') \
+                             .type_into_code_name_field('code')
             
             assert True            
         except urllib3.exceptions.MaxRetryError:            
             assert True    
         except Exception as ex:            
-            log_exception(ex, HEADLESS_MODE, drive_test_enroll_page, 'enroll_page')
+            log_exception(ex, HEADLESS_MODE, drive_enroll_page, 'enroll_page')
             pytest.fail
 
 # ==========================  Init tests  =====================================
 
-MAIN_PAGE_URL = data['pages']['enroll_page']
+ENROLL_PAGE_URL = data['pages']['enroll_page']
 BROWSERS = browsers_config
 HEADLESS_MODE = headless_mode_config
 
 # Test on all browsers
 for browser in BROWSERS:
-    drive_test_enroll_page = DriveEnrollPage(browser, MAIN_PAGE_URL, HEADLESS_MODE)
+    DriverHandler.init_driver(browser, HEADLESS_MODE)
+    drive_enroll_page = DriveEnrollPage(DriverHandler.get_driver(), ENROLL_PAGE_URL)
 
     # Fill forms fields
     TestEnrollPage.test_form()
             
-    drive_test_enroll_page.__exit__()
+    drive_enroll_page.__exit__()
